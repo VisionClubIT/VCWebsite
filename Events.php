f77f58c98e022e8b03c2bf69669466a72a1b0f7e
Kadi AlDaffaa <ksuvc.it@gmail.com>
2023-11-06 16:29:38 +0300

    capitalisation

--- Events.php
+++ Events.php
@@ -28,7 +28,7 @@ if ($r && mysqli_num_rows($r) > 0) {
 <html lang="ar" dir="rtl">
 
 <head>
-    <?php include_once 'Header/headerhead.php';?>
+    <?php include_once 'Header/HeaderHead.php';?>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -40,7 +40,7 @@ if ($r && mysqli_num_rows($r) > 0) {
 </head>
 
 <body class="bg-white" style="margin:0">
-    <?php include_once 'Header/headerbody.php';?>
+    <?php include_once 'Header/HeaderBody.php';?>
     <div class="promotional-event" style="background-image: url('<?php echo $backgroundImage; ?>'); ">
         <div class="promotional-overlay"></div> <!-- Grey overlay for text -->
         <div class="promo-rectangle">
@@ -77,7 +77,7 @@ if ($r && mysqli_num_rows($r) > 0) {
         </div>
     </div>
 
-    <?php include_once 'Footer/footerbody.php' ?>
+    <?php include_once 'Footer/footerBody.php' ?>
 
 </body>
 

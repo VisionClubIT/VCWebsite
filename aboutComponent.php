a5a6bd89659f941c615797e7e06ee4f38b1b7ab6
Kadi AlDaffaa <ksuvc.it@gmail.com>
2023-11-06 23:21:51 +0300

    capitalization

--- aboutComponent.php
+++ aboutComponent.php
@@ -94,7 +94,7 @@ p {
   <title>Component</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
 <div class="component-header">

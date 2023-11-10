5d316fa3029f783359d0f54a234fe365a3e95a5d
saadiya <saadiyaa65@gmail.com>
2023-11-10 16:23:49 +0300

    login page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>
+            Login
+        </title>
+        <link rel="stylesheet" href="loginstyle.css">
+    </head>
+
+    <body>
+        <div class="card">
+           <img src="media/Logo.color.png" class="logo">
+           <div class="input">
+           <input type="email" placeholder="email" class="email">
+           <br>
+           <input type="password" placeholder="password" class="password">
+           <br>
+           <a href=verfication.php class="forgot"> forgot password?</a>
+           <br>
+           <button class="login-button"> Log in</button>
+           </div>
+        </div>
+
+    </body>
+</html>
---
+++ verfication.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>
+            verfication
+        </title>
+        <link rel="stylesheet" href="loginstyle.css">
+    </head>
+
+    <body>
+        <div class="card">
+           <img src="media/Logo.color.png" class="logo">
+           <div class="input">
+           <input type="email" placeholder="email" class="email">
+           <br>
+           <button class="login-button"> Verify</button>
+           </div>
+        </div>
+
+    </body>
+</html>
